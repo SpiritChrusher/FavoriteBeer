8f01e52d3ecf9235d9b49f898ba6815be56b7a17
SpiritChrusher <szabokristof997@gmail.com>
2020-05-21 02:43:52 +0200

    UI changes

--- src/main/java/org/kristof/SearchController.java
+++ src/main/java/org/kristof/SearchController.java
@@ -1,11 +1,15 @@
 package org.kristof;
 
 import Backend_Beer.Beertypes;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
@@ -18,29 +22,31 @@ public class SearchController {
 
 
 
-@FXML
-public ListView listshow;
+    @FXML
+    private ListView listshow;
 
+    @FXML
+    private ListView listchosen;
 
-   /* private static Scene scene;
+    @FXML
+    private Button search;
 
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("Search"),800, 600);
-        stage.setScene(scene);
-        stage.show();
+    @FXML
+    private Button move;
+
+    @FXML
+    private Label found;
+
+    @FXML
+    public void initialize()
+    {
+   // listshow.setPrefHeight(500);
+        populate(listshow);
+        populate(listchosen);
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
-*/
-public void populate()
-{
+
+    public void populate(ListView a) {
     ArrayList<String> lista = new ArrayList<>();
 
     for (var i : Beertypes.values())
@@ -48,8 +54,12 @@ public void populate()
         lista.add(i.getName());
     }
 
-
-    listshow.getItems().addAll(lista);
+    a.getItems().addAll(lista);
 }
 
+    public void Move(ActionEvent actionEvent) {
+    }
+
+    public void Search(ActionEvent actionEvent) {
+    }
 }
